4ced9f7d8900c384083d315c8f4f0c47eab48816
ykt74409495190879906 <952722763@qq.com>
2019-09-26 11:26:42 +0800

    1

---
+++ gitlibrary/src/main/java/com/xinyartech/gitlibrary/Constants2.java
@@ -0,0 +1,12 @@
+package com.xinyartech.gitlibrary;
+
+/**
+ * <pre>
+ *     author  : QB
+ *     time    : 2019/9/26
+ *     version : v1.0.0
+ *     desc    :
+ * </pre>
+ */
+public class Constants2 {
+}
