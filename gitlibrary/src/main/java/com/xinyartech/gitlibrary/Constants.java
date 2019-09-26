83a4b409359d914902e14890437d3d4233b41c82
ykt74409495190879906 <952722763@qq.com>
2019-09-26 09:37:39 +0800

    Initial commit

---
+++ gitlibrary/src/main/java/com/xinyartech/gitlibrary/Constants.java
@@ -0,0 +1,13 @@
+package com.xinyartech.gitlibrary;
+
+/**
+ * <pre>
+ *     author  : QB
+ *     time    : 2019/9/26
+ *     version : v1.0.0
+ *     desc    :
+ * </pre>
+ */
+public class Constants {
+    public static final String url = "http://wwww.baidu.com";
+}
